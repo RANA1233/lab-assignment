b8f8dddca04d7b9c89eed32819721c0bc9fc24c1
RANA1233 <104382693+RANA1233@users.noreply.github.com>
2022-04-25 23:04:34 +0530

    Add files via upload

---
+++ output.py
@@ -0,0 +1,24 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=========== RESTART: C:\Users\ASUS\Python assignment.py ==========
+
+enter number:8
+enter number:15
+enter number:78
+average of numbers = 33.666666666666664
+
+Enter Gross Income: 4567890
+Enter number of dependents : 4
+Taxable Income: 4545890
+Tax: 909178.0
+
+student:  ['SID', 'Name', 'Gender', 'Department Name', 'CGPA']
+student:  [21102067, 'RAKSHIT', 'M', 'CIVIL', '10.0']
+
+Marks Before Sorting:  [66, 78, 33, 67, 90, 12]
+Marks After Sorting: [12, 33, 66, 67, 78, 90]
+
+color ['red', 'green', 'white', 'black', 'pink', 'yellow']
+(a) color ['red', 'green', 'white', 'pink', 'yellow']
+(b) color ['red', 'green', 'white', 'purple', 'yellow']
