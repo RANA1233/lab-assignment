6f3db3253459be2a089122bf08c46e4029620209
RANA1233 <104382693+RANA1233@users.noreply.github.com>
2022-06-12 18:46:29 +0530

    Add files via upload

---
+++ Assignment 4/Q1.py
@@ -0,0 +1,15 @@
+a = int(input('Enter a number: '))
+if(a<=25):
+ print("The Grade Awarded Is F")
+elif(a<=45):
+ print("The Grade Awarded Is E")
+elif(a<=50):
+ print("The Grade Awarded Is D")
+elif(a<=60):
+ print("The Grade Awarded Is C")
+elif(a<=80):
+ print("The Grade Awarded Is B")
+elif(a<80):
+ print("The Grade Awarded Is A") 
+elif(a>100):
+ print("Enter Value From 1 To 100")
